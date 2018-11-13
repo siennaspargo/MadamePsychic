01a9baca00f2881eeb9d6b6333da60e10df92692
SiennaS <ssienna48@gmail.com>
2018-11-12 22:19:08 -0500

    looking better

--- Assets/javascript/code.js
+++ Assets/javascript/code.js
@@ -1,39 +1,54 @@
-            // a list of psychic's correct answers 
+
             var choices = ['a', 'b', 'c', 'd', 'e', 'f', 'u', 'v', 'w', 'x', 'y', 'z'];
-
-            // lettersToGuess variable will be randomly assigned one of the letter choices
             var lettersToGuess = null;
-
-
-            // an array to hold what has been guessed
             var guessesSoFar = [];
-
-            // to hold the count down of guesses
             var guessesLeft = 9;
-
-            // A Counter for wins/losses
             var wins = 0;
             var losses = 0;
+            //_____________________________________________
+
+            // function to capture letters pressed
+            document.onkeyup = function (event) {
+                // reduce guess by 1
+                guessesLeft--;
+
+                var userGuessLower = String.fromCharCode(event.which).toLowerCase();
 
 
-            //  3 functions updateGuessesLeft, updateLetterToGuess and updateGuessesSoFar
+                guessesSoFar.push(userGuessLower);
+
+                updateGuessesLeft();
+                updateGuessesSoFar();
+
+                // if the letter is a match
+                if (userGuessLower === letterToGuess) {
+                    wins++;
+                    document.querySelector("#wins").innerHTML = wins;
+                    reset();
+                } 
+                if (guessesLeft === 0) {
+                    losses++;
+                    document.querySelector("#losses").innerHTML = losses;
+                    reset();
+                }
+            };
+
+            //_____________________________________________
 
             var updateGuessesLeft = function () {
                 document.querySelector("#guesses-left").textContent = guessesLeft;
             };
 
-            // updateLetterToGuess
             var updateLetterToGuess = function () {
                 letterToGuess = choices[Math.floor(Math.random() * choices.length)];
             };
 
-            // updateGuessesSoFar
             var updateGuessesSoFar = function () {
                 document.querySelector("#guesses-so-far").textContent = guessesSoFar.join(" , ");
             };
 
+             //_____________________________________________
 
-            //  a function to be called when reset takes place
             var reset = function () {
                 guessesLeft = 9;
                 guessedLetters = [];
@@ -41,44 +56,7 @@
                 updateGuessesLeft();
                 updateGuessesSoFar();
             };
-
-            // Execute on page load/ refresh page
+            
             updateLetterToGuess();
             updateGuessesLeft();
-
  
-            // function to capture letters pressed
-            document.onkeyup = function (event) {
-                // reduce guess by 1
-                guessesLeft--;
-
-                // makes letter pressed lowercase
-                var userGuessLower = String.fromCharCode(event.which).toLowerCase();
-
-                // to add the guess to the guessedLetters Array
-                guessesSoFar.push(userGuessLower);
-
-                // run the update functions
-                updateGuessesLeft();
-                updateGuessesSoFar();
-
-                // Check to see if the letter is a match
-                if (userGuessLower === letterToGuess) {
-
-                    // if matched, count as win and update HTML to display the win
-                    wins++;
-                    document.querySelector("#wins").innerHTML = wins;
-
-                    // Reset the game
-                    reset();
-                } 
-                if (guessesLeft === 0) {
-
-                    // then the user lost and the HTML needs to update to display the loss
-                    losses++;
-                    document.querySelector("#losses").innerHTML = losses;
-
-                    // reset game
-                    reset();
-                }
-            };
